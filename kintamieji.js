1971ba21b48c68fdcbe1e1004e4eb7c143c25736
Karolis <karolis.luko@gmail.com>
2021-06-09 14:24:43 +0100

    updete-3

--- kintamieji.js
+++ kintamieji.js
@@ -14,7 +14,28 @@ var temp;
 temp = skaicius1;//=2
 skaicius1 = skaicius2;//=3
 skaicius2 = temp;//=2
+console.log(`skaicius1 = ${skaicius1} skaicius2 = ${skaicius2}`);
+
 
 skaicius3 = skaicius3 + skaicius4;//skaicius3 = 9 
 skaicius4 = skaicius3 - skaicius4;//skaicius4 = 4
 skaicius3 = skaicius3 - skaicius4;//skaicius3 = 5
+console.log(`skaicius3 = ${skaicius3}  skaicius4 = ${skaicius4} `);
+
+var maxSkaicius;
+maxSkaicius = Number.MAX_SAFE_INTEGER + 1;
+console.log(maxSkaicius);
+
+
+var skaicius5, skaicius6;
+
+var skaicius5 = 6;
+var skaicius6 = 8;
+
+skaicius5 = skaicius5 ^ skaicius6;
+skaicius6 = skaicius5 ^ skaicius6;
+skaicius5 = skaicius5 ^ skaicius6;
+console.log(`skaicius5 =${skaicius5} skaicius6 = ${skaicius6}`)
+
+var element = document.getElementById('Rezultatas');
+element.innerHTML = `skaicius5 =${skaicius5} skaicius6 = ${skaicius6}`
