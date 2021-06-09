d5459fef6f3c32fae41a89df5e0471e0402797ff
Karolis <karolis.luko@gmail.com>
2021-06-09 08:55:42 +0100

    updete

---
+++ kintamieji.js
@@ -0,0 +1,20 @@
+"use strict"
+var skaicius1, skaicius2, skaicius3, skaiciu4;
+
+var skaicius1 = 2;
+var skaicius2 = 3;
+var skaicius3 = 4;
+var skaicius4 = 5;
+
+console.log (skaicius1 + skaicius2 + skaicius3 + skaicius4);
+console.log (skaicius1 * skaicius2 * skaicius3 * skaicius4);
+console.log(skaicius1, skaicius2, skaicius3, skaicius4);
+
+var temp;
+temp = skaicius1;
+skaicius1 = skaicius2;
+skaicius2 = temp;
+
+skaicius3 += skaicius4; 
+skaicius4 = skaicius3 - skaicius4;
+skaicius3 -= skaicius4;
